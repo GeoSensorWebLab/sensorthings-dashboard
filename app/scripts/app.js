e8dba604f09c14bc72609c4922602ea25c086305
James Badger <jpbadger@ucalgary.ca>
2016-05-05 14:14:09 -0600

    Extract ParamsController to class

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,47 +3,7 @@
 import './chart';
 import './color_generator';
 import './notifier';
+import ParamsController from './params_controller';
 import './settings';
 
-var activeParams = new Map();
-
-window.ParamsController = {
-  activate() {
-    activeParams = this.parse();
-
-    if (activeParams.get("stURL") === undefined) {
-      this.set("stURL", Settings.getSensorThingsURL());
-    }
-  },
-
-  encodedParams() {
-    var params = [];
-    activeParams.forEach(function(value, key) {
-      params.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
-    });
-
-    return params.join("&");
-  },
-
-  // Retrieve current URL query parameters as a hash
-  parse() {
-    var params = new Map();
-
-    if (window.location.search === "") { return params; }
-
-    var encodedParams = window.location.search.split('?')[1];
-    encodedParams.split('&').forEach(function(pair) {
-      var parts = pair.split('=');
-      var key = decodeURIComponent(parts[0]),
-        value = decodeURIComponent(parts[1]);
-      params.set(key, value);
-    });
-
-    return params;
-  },
-
-  set(key, value) {
-    activeParams.set(key, value);
-    history.replaceState({}, "", location.pathname + "?" + this.encodedParams());
-  }
-};
+window.ParamsController = new ParamsController({ settings: Settings });
---
+++ app/scripts/params_controller.js
@@ -0,0 +1,49 @@
+// Parse query parameters in the URL
+// Adds values from localStorage if they are missing in the query parameters.
+class ParamsController {
+  constructor(options) {
+    this.activeParams = new Map();
+    this.settings = options.settings;
+  }
+
+  activate() {
+    this.activeParams = this.parse();
+
+    if (this.activeParams.get("stURL") === undefined) {
+      this.set("stURL", this.settings.getSensorThingsURL());
+    }
+  }
+
+  encodedParams() {
+    var params = [];
+    this.activeParams.forEach(function(value, key) {
+      params.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+    });
+
+    return params.join("&");
+  }
+
+  // Retrieve current URL query parameters as a hash
+  parse() {
+    var params = new Map();
+
+    if (window.location.search === "") { return params; }
+
+    var encodedParams = window.location.search.split('?')[1];
+    encodedParams.split('&').forEach(function(pair) {
+      var parts = pair.split('=');
+      var key = decodeURIComponent(parts[0]),
+        value = decodeURIComponent(parts[1]);
+      params.set(key, value);
+    });
+
+    return params;
+  }
+
+  set(key, value) {
+    this.activeParams.set(key, value);
+    history.replaceState({}, "", location.pathname + "?" + this.encodedParams());
+  }
+}
+
+export default ParamsController;
