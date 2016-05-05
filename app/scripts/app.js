5b55ce494c3b993fd595485b02e6c241cc6f4f0f
James Badger <jpbadger@ucalgary.ca>
2016-05-05 13:31:29 -0600

    Extract color generator to module file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,6 @@
 // Start with your ES5/6/7 script here.
 // Import other files or use Browserify require statements.
-
-function hashFnv32a(str, asString, seed) {
-  /*jshint bitwise:false */
-  var i, l,
-  hval = (seed === undefined) ? 0x811c9dc5 : seed;
-
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-  }
-  if(asString) {
-    // Convert to 8 digit hex string
-    return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
-  }
-  return hval >>> 0;
-}
-
-// return a deterministic line color for a given number.
-// This causes each datastream to always receive the same line colour.
-window.colorForId = function(id) {  
-  return hashFnv32a(id.toString(), true).substr(2, 8);
-};
+import './color_generator';
 
 window.sampleChartData = function(options) {
   var sin = [];
