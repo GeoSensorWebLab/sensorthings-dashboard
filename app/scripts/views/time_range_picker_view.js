0e237ed0017d51ee48e04008f48f1bf31d475edd
James Badger <jpbadger@ucalgary.ca>
2016-05-09 16:52:40 -0600

    Add onChange callback for Time Picker
    Also fix time formatting bug causing 24 hour to not display
    correctly in time picker.

--- app/scripts/views/time_range_picker_view.js
+++ app/scripts/views/time_range_picker_view.js
@@ -1,12 +1,14 @@
-var ISODateFormat = 'YYYY-MM-DD[T]hh:mm:ssZ';
+var ISODateFormat = 'YYYY-MM-DD[T]HH:mm:ssZ';
 
 // Draw a time range picker in an HTML element.
 // Will auto-render. Uses moment-fork of JQuery DateTimePicker.
 // `elementSelector` - CSS selector for the container element
+// `onChange` - function callback triggered when time range is changed and
+//              applied
 // `options.startDate` - moment date for the start date
 // `options.endDate` - moment date for the end date
 class TimeRangePickerView {
-  constructor(elementSelector, options = {}) {
+  constructor(elementSelector, onChange = function() {}, options = {}) {
     this.$el = $(elementSelector);
     this.startDate = options.startDate || moment().subtract(12, 'hours');
     this.endDate   = options.endDate || moment();
@@ -18,23 +20,36 @@ class TimeRangePickerView {
     var $template = JST["time-range-picker"]();
     this.$el.html($template);
 
-    this.$el.find('.start-time').text(this.startDate.local());
-    this.$el.find('.end-time').text(this.endDate.local());
+    this.$el.find('.start-date').text(this.startDate.local());
+    this.$el.find('.end-date').text(this.endDate.local());
 
-    this.$el.find('[name="start-date"]').val(this.startDate.format(ISODateFormat));
-    this.$el.find('[name="end-date"]').val(this.endDate.format(ISODateFormat));
+    this.$el.find('.start-date-control').val(this.startDate.format(ISODateFormat));
+    this.$el.find('.end-date-control').val(this.endDate.format(ISODateFormat));
 
     // Apply custom datetime picker for clients who do not render their own
     if (this.$el.find('[type="datetime"]').prop('type') !== 'datetime') {
       this.$el.find('[type="datetime"]').datetimepicker({
         format: ISODateFormat,
-        formatTime: 'hh:mm',
-        formatDate: 'YYYY-MM-DD'
+        formatTime: 'HH:mm',
+        formatDate: 'YYYY-MM-DD',
+        hours12: false
       });
     }
 
-    $(".start-time, .end-time").on("click", () => {
+    $(".start-date, .end-date").on("click", () => {
       $(".time-range-picker-controls").show();
+    });
+
+    $("button.apply-time-range").on("click", () => {
+      // TODO: Add date validation check
+      this.startDate = moment(this.$el.find('.start-date-control').val());
+      this.endDate   = moment(this.$el.find('.end-date-control').val());
+      $(".time-range-picker-controls").hide();
+
+      this.$el.find('.start-date').text(this.startDate.local());
+      this.$el.find('.end-date').text(this.endDate.local());
+
+      this.onChange(this.startDate, this.endDate);
     });
   }
 }
