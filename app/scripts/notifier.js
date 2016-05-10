8742a1114437f5c442cc4cb7ee6a3a37ddb72001
James Badger <jpbadger@ucalgary.ca>
2016-05-10 12:19:11 -0600

    Add clear method

--- app/scripts/notifier.js
+++ app/scripts/notifier.js
@@ -6,18 +6,22 @@ class Notifier {
     this.$element = $(elementSelector);
   }
 
-  error(msg) {
+  clear() {
     this.$element.children("div").hide();
+  }
+
+  error(msg) {
+    this.clear();
     this.$element.find(".error").show().html(msg);
   }
 
   success(msg) {
-    this.$element.children("div").hide();
+    this.clear();
     this.$element.find(".success").show().html(msg);
   }
 
   warning(msg) {
-    this.$element.children("div").hide();
+    this.clear();
     this.$element.find(".warning").show().html(msg);
   }
 }
