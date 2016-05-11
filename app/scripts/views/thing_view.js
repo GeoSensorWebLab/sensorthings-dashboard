0442223a6831ecf0e9657b2bafc40683a767a9de
James Badger <jpbadger@ucalgary.ca>
2016-05-11 16:34:34 -0600

    Extract methods from ThingView constructor

--- app/scripts/views/thing_view.js
+++ app/scripts/views/thing_view.js
@@ -2,6 +2,8 @@ import ResultView from './result_view';
 import StaticMap from '../maps/static_map';
 import TimeRangePickerView from './time_range_picker_view';
 
+// View class for page showing a single Thing and a list of Datastreams. Each
+// Datastream will render a ResultView.
 class ThingView {
   constructor() {
     // Update query params in URL, get SensorThings URL from query params
@@ -15,59 +17,27 @@ class ThingView {
       }
     });
 
-    // Map
-    var MapManager = new StaticMap('map');
-    MapManager.map.setView([51.049, -114.08], 13);
+    this.MapManager = null;
+    this.initMap();
 
     // Time Range Picker
-    var dateRange = Q.defer();
-    var dateChangeFunctions = [];
-
-    new TimeRangePickerView("#time-range-picker", {
-      onChange: function(startDate, endDate) {
-        dateChangeFunctions.forEach(function(fn) {
-          fn.apply(this, [startDate, endDate]);
-        });
-      },
-
-      onLoad: function(startDate, endDate) {
-        dateRange.resolve({ startDate: startDate, endDate: endDate });
-      }
-    });
+    this.dateRange = Q.defer();
+    this.dateChangeFunctions = [];
+    this.initTimeRangePicker();
 
     // Data Load Handler
-    Q(Thing).then(function(thing) {
+    Q(Thing).then((thing) => {
       // Load Metadata
-      var properties = $.extend({}, thing.attributes, {
-        locationDescription: thing.locations[0].get("description")
-      });
-      var template = JST["thing-info"](properties);
-      $("#thing-info").html(template);
-      $("#thing-description").html(thing.get("description"));
+      this.renderMetadata(thing);
 
       // Load feature marker
       Q(thing.locations)
-      .then(function(locations) {
+      .then((locations) => {
         var location = locations[0];
         if (location && location.get("location")) {
-          // Generate HTML for popup
-          var properties = $.extend({}, thing.attributes, {
-            stURL: encodeURIComponent(App.ParamsController.get("stURL"))
-          });
-          var template = JST["marker-popup"](properties);
-
-          // We use circle markers as we can style them with path CSS.
-          var createMarker = function(feature, latlng) {
-            var marker = L.circleMarker(latlng);
-            marker.bindPopup(template);
-            return marker;
-          };
-
-          var feature = L.geoJson(location.get("location"), {
-            pointToLayer: createMarker
-          }).addTo(MapManager.map);
-          thing.set("mapFeature", feature);
-          MapManager.map.fitBounds(feature.getBounds());
+          this.addToMap(thing, location);
+        } else {
+          console.warn("Location could not be added to Map. Check `location` attribute.");
         }
       });
 
@@ -77,37 +47,92 @@ class ThingView {
           "$expand": "Sensor,ObservedProperty"
         }
       }))
-      .then(function(datastreams) {
-        datastreams.forEach(function(datastream) {
-          // Draw template for each datastream
-          var $template = $(JST["datastream-card"](datastream.attributes));
-          $("#datastreams").append($template);
+      .then((datastreams) => {
+        datastreams.forEach(this.renderDatastream, this);
+      })
+      .done();
+    })
+    .done();
+  }
 
-          var sensorTemplate = JST["sensor-info"](datastream.sensor.attributes);
-          $template.find(".sensor-attributes").append(sensorTemplate);
+  addToMap(thing, location) {
+    // Generate HTML for popup
+    var properties = $.extend({}, thing.attributes, {
+      stURL: encodeURIComponent(App.ParamsController.get("stURL"))
+    });
+    var template = JST["marker-popup"](properties);
 
-          var propertyTemplate = JST["observed-property-info"](datastream.observedProperty.attributes);
-          $template.find(".observed-property-attributes").append(propertyTemplate);
+    // We use circle markers as we can style them with path CSS.
+    var createMarker = function(feature, latlng) {
+      var marker = L.circleMarker(latlng);
+      marker.bindPopup(template);
+      return marker;
+    };
 
-          // Draw Results
-          Q(dateRange.promise)
-          .then((dateRange) => {
-            var id = datastream.get("@iot.id");
+    var feature = L.geoJson(location.get("location"), {
+      pointToLayer: createMarker
+    }).addTo(this.MapManager.map);
+    thing.set("mapFeature", feature);
+    this.MapManager.map.fitBounds(feature.getBounds());
+  }
 
-            var view = new ResultView(datastream, `#datastream-${id}-card`, {
-              startDate: dateRange.startDate,
-              endDate: dateRange.endDate
-            });
+  initMap() {
+    this.MapManager = new StaticMap('map');
+    this.MapManager.map.setView([51.049, -114.08], 13);
+  }
 
-            dateChangeFunctions.push(function(startDate, endDate) {
-              view.update(startDate, endDate);
-            });
-          })
-          .done();
+  // Create the Time Range Picker.
+  // Will resolve a Deferred Promise object for this.dateRange that can be used
+  // for callbacks in other functions.
+  initTimeRangePicker() {
+    new TimeRangePickerView("#time-range-picker", {
+      onChange: (startDate, endDate) => {
+        this.dateChangeFunctions.forEach((fn) => {
+          fn.apply(this, [startDate, endDate]);
         });
+      },
+
+      onLoad: (startDate, endDate) => {
+        this.dateRange.resolve({ startDate: startDate, endDate: endDate });
+      }
+    });
+  }
+
+  renderDatastream(datastream) {
+    // Draw template for each datastream
+    var $template = $(JST["datastream-card"](datastream.attributes));
+    $("#datastreams").append($template);
+
+    var sensorTemplate = JST["sensor-info"](datastream.sensor.attributes);
+    $template.find(".sensor-attributes").append(sensorTemplate);
+
+    var propertyTemplate = JST["observed-property-info"](datastream.observedProperty.attributes);
+    $template.find(".observed-property-attributes").append(propertyTemplate);
+
+    // Draw Results
+    return Q(this.dateRange.promise)
+    .then((dateRange) => {
+      var id = datastream.get("@iot.id");
+
+      var view = new ResultView(datastream, `#datastream-${id}-card`, {
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate
+      });
+
+      this.dateChangeFunctions.push((startDate, endDate) => {
+        view.update(startDate, endDate);
       });
     })
     .done();
+  }
+
+  renderMetadata(thing) {
+    var properties = $.extend({}, thing.attributes, {
+      locationDescription: thing.locations[0].get("description")
+    });
+    var template = JST["thing-info"](properties);
+    $("#thing-info").html(template);
+    $("#thing-description").html(thing.get("description"));
   }
 }
 
